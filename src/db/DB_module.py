21ca7001bfec49fbf5659d629155bd00f8f91069
thisislok <lokssocial.com>
2025-11-15 22:01:54 +0000

    add 2 scripts, 1 for modules, 1 for fetching data from DB

---
+++ src/db/DB_module.py
@@ -0,0 +1,59 @@
+#Make sure you have "ODBC Driver 17 for SQL Server" installed on your machinem link:
+#  https://learn.microsoft.com/en-us/sql/connect/odbc/download-odbc-driver-for-sql-server
+
+import pyodbc
+from datetime import datetime
+
+#user_log is a prodcedure created for main scripts that returns no value back
+#It is a one-way street that send the data to the Azure SQL DB. The way to review the data is writen to logs_monitor.py
+
+#assume the placeholder containing the weather info from the main script is called "data" 
+def user_log(data):
+
+    connection = pyodbc.connect("""
+                            DRIVER={ODBC Driver 17 for SQL Server};
+                            SERVER=temp-project.database.windows.net;
+                            DATABASE=weatherdb;
+                            UID=mthreeproject;
+                            PWD=m3m3MTHREE;
+                            """)
+
+    cursor = connection.cursor()
+
+#Automation for table creation. With IF NOT EXISTS error handling. It would only create once in first run, avoding repeat creation.
+    cursor.execute("""
+        IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='logHist')
+        BEGIN
+                              
+            CREATE TABLE logHist (
+                   
+            id INT IDENTITY PRIMARY KEY,
+            location NVARCHAR(100),
+            temp_c FLOAT,
+            wind_mph FLOAT,
+            precip_mm FLOAT,
+            humidity INT,
+            cloud INT,
+            timestamp DATETIME DEFAULT GETDATE() 
+        )
+        END                       
+    """)
+
+    connection.commit()
+#automation for logs insertion
+    cursor.execute("""
+        INSERT INTO logHist (location, temp_c, wind_mph, precip_mm, humidity, cloud)
+        VALUES (?, ?, ?, ?, ?, ?)
+    """, (
+        data.get("location"),
+        data.get("temp_c"),
+        data.get("wind_mph"),
+        data.get("precip_mm"),
+        data.get("humidity"),
+        data.get("cloud"),
+    ))
+
+    connection.commit()
+    connection.close()
+
+#Completely disconnect to the DB server
---
+++ src/db/logs_monitor.py
@@ -0,0 +1,45 @@
+# Run this script to review all existing search logs stored in Azure SQL DB
+import pyodbc
+
+try:
+    connection = pyodbc.connect("""
+                            DRIVER={ODBC Driver 17 for SQL Server};
+                            SERVER=temp-project.database.windows.net;
+                            DATABASE=weatherdb;
+                            UID=mthreeproject;
+                            PWD=m3m3MTHREE;
+                            """)
+    
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM logHist")
+
+    print("\n Logs - Users' browsing history for weather info \n")
+
+    columns = [column[0] for column in cursor.description]
+
+    while True:
+        row = cursor.fetchone()
+        if not row:
+            break
+
+        for col_name, value in zip(columns, row):
+            print(f"{col_name} | {value}")
+
+    cursor.close()
+    connection.close()
+    print("Connection closed.")
+
+except pyodbc.Error as ex:
+    print("\nException:", ex)
+    print("Closing program...")
+
+    try:
+        cursor.close()
+    except:
+        pass
+    try:
+        connection.close()
+    except:
+        pass
+
+    exit()
